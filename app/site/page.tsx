924ed05e680dc178e93fdb838fd2919951f83013
jtr-code <jtrtricks@gmail.com>
2024-11-01 01:20:20 +0530

    fix onchange

--- app/site/page.tsx
+++ app/site/page.tsx
@@ -1,8 +1,35 @@
-import React from "react";
+
+"use client";
+
+import React, { useState } from "react";
 
 const Site = () => {
+
+    const [formData, setFormData] = useState({
+        title: "",
+        description: '',
+        image: '',
+        url: '',
+        author: "",
+        isTrending: false,
+    })
+
+    const handleChange = (e: any) => {
+        const { name, type, checked, value } = e.target;
+
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: type === 'checkbox' ? checked : value,
+        }));
+    };
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+        console.log('submitted')
+    }
+
     return (
-        <form className="px-8 pt-6 pb-8 mb-4 space-y-6 min-h-screen">
+        <form className="px-8 pt-6 pb-8 mb-4 space-y-6 min-h-screen" onSubmit={handleSubmit}>
             <div className="space-y-2">
                 <label
                     htmlFor="title"
@@ -11,23 +38,27 @@ const Site = () => {
                     Title*
                 </label>
                 <input
+                    value={formData.title}
                     type="text"
                     placeholder="Title of your site"
                     name="title"
+                    onChange={handleChange}
                     className="w-full px-3 py-2 bg-slate-600 border border-slate-500 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:border-slate-400 text-gray-200"
                 />
             </div>
 
             <div className="space-y-2">
                 <label
-                    htmlFor="body"
+                    htmlFor="description"
                     className="block text-sm font-medium text-gray-200"
                 >
                     Description*
                 </label>
                 <textarea
                     placeholder="Add your description"
-                    name="body"
+                    name="description"
+                    value={formData.description}
+                    onChange={handleChange}
                     rows={4}
                     className="w-full px-3 py-2 bg-slate-600 border border-slate-500 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:border-slate-400 text-gray-200"
                 />
@@ -41,9 +72,11 @@ const Site = () => {
                     Select image:*
                 </label>
                 <input
+                    value={formData.image}
                     type="file"
                     id="img"
-                    name="img"
+                    name="image"
+                    onChange={handleChange}
                     accept="image/*"
                     className="w-full text-sm outline-none text-gray-200 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-medium file:bg-slate-600 file:text-gray-100 hover:file:bg-slate-700"
                 />
@@ -57,6 +90,8 @@ const Site = () => {
                     Url
                 </label>
                 <input
+                    value={formData.url}
+                    onChange={handleChange}
                     type="text"
                     placeholder="Link of your site"
                     name="url"
@@ -72,6 +107,8 @@ const Site = () => {
                     Author
                 </label>
                 <input
+                    value={formData.author}
+                    onChange={handleChange}
                     type="text"
                     placeholder="Author name"
                     name="author"
@@ -81,7 +118,13 @@ const Site = () => {
 
             <div className="flex items-center space-x-3">
                 <label className="relative inline-flex items-center cursor-pointer">
-                    <input type="checkbox" id="checkbox" className="sr-only peer" />
+                    <input
+                        checked={formData.isTrending}
+                        type="checkbox"
+                        onChange={handleChange}
+                        name="isTrending"
+                        id="checkbox"
+                        className="sr-only peer" />
                     <div className="w-11 h-6 bg-slate-600 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-slate-800 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-slate-500 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-slate-700"></div>
                 </label>
                 <span className="text-sm font-medium text-gray-200">
@@ -91,6 +134,7 @@ const Site = () => {
 
             <div className="pt-4">
                 <button
+
                     type="submit"
                     className="w-fit bg-slate-600 hover:bg-slate-700 text-white font-medium py-2 px-4 rounded-md shadow-sm transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2 focus:ring-offset-slate-700"
                 >
