898513f3f38c35297586666f9b78b27e35220f1a
jtr-code <jtrtricks@gmail.com>
2024-11-10 23:01:06 +0530

    add : create site url

--- app/LayoutWrapper.tsx
+++ app/LayoutWrapper.tsx
@@ -1,13 +1,17 @@
+"use client";
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import { SnackbarProvider } from "notistack";
 
 const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <main className=" w-full ">{children}</main>
-    </div>
-  );
+    return (
+        <SnackbarProvider>
+            <div className="flex min-h-screen">
+                <Sidebar />
+                <main className=" w-full ">{children}</main>
+            </div>
+        </SnackbarProvider>
+    );
 };
 
 export default LayoutWrapper;
--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,3 +1,5 @@
 export const BASE_URL = "http://localhost:3001/api/v1";
 
 export const GET_ALL_SITES = BASE_URL + "/sites";
+
+export const CREATE_SITE = BASE_URL + "/admin/site";
