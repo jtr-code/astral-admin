c6bb46527ca4bb04084140ccb8a19ba5dfc1e7bb
jtr-code <jtrtricks@gmail.com>
2024-11-12 23:30:48 +0530

    add : url

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -2,4 +2,8 @@ export const BASE_URL = "http://localhost:3001/api/v1";
 
 export const GET_ALL_SITES = BASE_URL + "/sites";
 
+export const GET_SITE_BY_ID = BASE_URL + "/sites"; // will pass siteId as params
+
 export const CREATE_SITE = BASE_URL + "/admin/site";
+
+export const UPDATE_SITE = BASE_URL + "/admin/site"; // will pass siteId as params
