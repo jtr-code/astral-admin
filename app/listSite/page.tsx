37fc7fca6ae19b33555c3375c6cc31b0d5045aca
jtr-code <jtrtricks@gmail.com>
2024-11-13 23:29:50 +0530

    add : handleEditButton skeleton

--- app/listSite/page.tsx
+++ app/listSite/page.tsx
@@ -13,6 +13,7 @@ interface SiteData {
     author: string;
     isTrending: boolean;
     updatedAt: string;
+    _id: string;
 }
 
 const SitesTable = () => {
@@ -33,6 +34,10 @@ const SitesTable = () => {
         }
         fetchPosts();
     }, []);
+
+    const handleEditButton = (siteId: string) => {
+        console.log(siteId)
+    }
 
     if (!sites || loading) return <Loader />;
     if (sites.length === 0) return <NoContentPage />;
@@ -98,11 +103,10 @@ const SitesTable = () => {
                                 <td className="px-4 py-3 text-gray-200">{site.author}</td>
                                 <td className="px-4 py-3">
                                     <span
-                                        className={`px-2 py-1 rounded-full text-xs ${
-                                            site.isTrending
+                                        className={`px-2 py-1 rounded-full text-xs ${site.isTrending
                                                 ? "bg-green-500/20 text-green-400"
                                                 : "bg-red-500/20 text-red-400"
-                                        }`}
+                                            }`}
                                     >
                                         {site.isTrending ? "True" : "False"}
                                     </span>
@@ -112,7 +116,7 @@ const SitesTable = () => {
                                 </td>
                                 <td className="px-4 py-3">
                                     <div className="flex space-x-2">
-                                        <button className="px-3 py-1 text-xs rounded-md bg-slate-600 text-gray-200 hover:bg-slate-500">
+                                        <button className="px-3 py-1 text-xs rounded-md bg-slate-600 text-gray-200 hover:bg-slate-500" onClick={() => handleEditButton(site._id)}>
                                             Edit
                                         </button>
                                         <button className="px-3 py-1 text-xs rounded-md bg-red-600 text-gray-200 hover:bg-red-500">
